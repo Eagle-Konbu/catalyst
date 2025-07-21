50f5cb0f1f71a14122a9981e7994acc61fc08881
Atsuya Uchida <atsuya_eagle@outlook.com>
2025-07-21 21:12:04 +0900

    Refactor light command structure for improved clarity and functionality

--- cmd/light.go
+++ cmd/light.go
@@ -14,50 +14,48 @@ import (
 // lightCmd represents the light command
 var lightCmd = &cobra.Command{
 	Use:   "light",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Control the light device",
+	Long:  `Control the light device using subcommands (on/off).`,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 || (args[0] != "on" && args[0] != "off") {
-			return fmt.Errorf("usage: light [on|off]")
-		}
-		return nil
-	},
+var onCmd = &cobra.Command{
+	Use:   "on",
+	Short: "Turn the light on",
 	Run: func(cmd *cobra.Command, args []string) {
-		if id == "" || token == "" {
-			fmt.Fprintln(cmd.ErrOrStderr(), "id or token is not set. Please check your config file or environment variables.")
-			os.Exit(1)
-		}
-
-		uc := usecase.NewLightUsecase(id, token)
-		var err error
-		if args[0] == "on" {
-			err = uc.TurnOnLight()
-		} else {
-			err = uc.TurnOffLight()
-		}
+		uc := getLightUsecaseOrExit(cmd)
+		err := uc.TurnOnLight()
 		if err != nil {
-			fmt.Fprintln(cmd.ErrOrStderr(), "failed to switch light:", err)
+			fmt.Fprintln(cmd.ErrOrStderr(), "failed to turn on light:", err)
 			os.Exit(1)
 		}
-		fmt.Printf("light has been turned %s successfully :)\n", args[0])
+		fmt.Println("light has been turned on successfully :)")
+	},
+}
+
+var offCmd = &cobra.Command{
+	Use:   "off",
+	Short: "Turn the light off",
+	Run: func(cmd *cobra.Command, args []string) {
+		uc := getLightUsecaseOrExit(cmd)
+		err := uc.TurnOffLight()
+		if err != nil {
+			fmt.Fprintln(cmd.ErrOrStderr(), "failed to turn off light:", err)
+			os.Exit(1)
+		}
+		fmt.Println("light has been turned off successfully :)")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(lightCmd)
+	lightCmd.AddCommand(onCmd)
+	lightCmd.AddCommand(offCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lightCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lightCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func getLightUsecaseOrExit(cmd *cobra.Command) *usecase.LightUsecase {
+	if id == "" || token == "" {
+		fmt.Fprintln(cmd.ErrOrStderr(), "id or token is not set. Please check your config file or environment variables.")
+		os.Exit(1)
+	}
+	return usecase.NewLightUsecase(id, token)
 }
