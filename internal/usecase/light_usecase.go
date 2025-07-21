8a599e816a7239df45842d98691d4f5eb1479661
Atsuya Uchida <atsuya_eagle@outlook.com>
2025-07-21 20:12:42 +0900

    Refactor LightUsecase to encapsulate id and api fields, and update method signatures for improved clarity

--- internal/usecase/light_usecase.go
+++ internal/usecase/light_usecase.go
@@ -3,18 +3,19 @@ package usecase
 import "github.com/Eagle-Konbu/catalyst/internal/infrastructure"
 
 type LightUsecase struct {
-	API *infrastructure.NatureRemoAPI
+	id  string
+	api *infrastructure.NatureRemoAPI
 }
 
-func NewLightUsecase(token string) *LightUsecase {
+func NewLightUsecase(id, token string) *LightUsecase {
 	api := infrastructure.NewNatureRemoAPI(token)
-	return &LightUsecase{API: api}
+	return &LightUsecase{id: id, api: api}
 }
 
-func (u *LightUsecase) TurnOnLight(id string) error {
-	return u.API.SwitchLight(id, "on")
+func (u *LightUsecase) TurnOnLight() error {
+	return u.api.SwitchLight(u.id, "on")
 }
 
-func (u *LightUsecase) TurnOffLight(id string) error {
-	return u.API.SwitchLight(id, "off")
+func (u *LightUsecase) TurnOffLight() error {
+	return u.api.SwitchLight(u.id, "off")
 }
